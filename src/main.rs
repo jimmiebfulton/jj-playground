e4f6bb6ef885c829453471efabb904788c233edd
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-04 20:27:37 -0800

    initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    print_mgs();
+    print_departure_message();
+}
+
+fn print_mgs() {
+    println!("Hello, World!");
+}
+
+fn print_departure_message() {
+    println!("Goodbye, world!");
 }
